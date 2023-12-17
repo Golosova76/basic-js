dbad634dd894282f46e05a9da217f069b4772b8c
Anna Golosova <golosova76@gmail.com>
2023-12-17 22:00:32 +0300

    feat: solution the two task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const strString = String(str);
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const addition = options.addition === undefined ? '' : String(options.addition);
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+  let interiumResult = [];
+  let result = [];
+  for (let i = 0; i < additionRepeatTimes; i +=1) {
+    interiumResult.push(addition);
+  }
+  for (let i = 0; i < repeatTimes; i +=1) {
+    result.push(strString + interiumResult.join(additionSeparator));
+  }
+  return result.join(separator);
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length != 0) {
+    throw new Error('Invalid date!');
+  }
+  const monthSeason = date.getMonth();
+  if (monthSeason === 1 || monthSeason === 0 || monthSeason === 11) return 'winter';
+  if (monthSeason >= 2 && monthSeason <= 4) return 'spring';
+  if (monthSeason >= 5 && monthSeason <= 7) return 'summer';
+  if (monthSeason >= 8 && monthSeason <= 10) return 'autumn' || 'fall';
 }
 
 module.exports = {
