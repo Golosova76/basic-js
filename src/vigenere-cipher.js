246c00eee384005581dc8dcba2efd76690e82c35
Anna Golosova <golosova76@gmail.com>
2023-12-25 00:36:49 +0300

    feat: solution the task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.reverse = !reverse;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  shiftChar(char, shift, encrypt = true) {
+    if (this.alphabet.includes(char.toUpperCase())) {
+        let index = this.alphabet.indexOf(char.toUpperCase());
+        shift = encrypt ? shift : -shift;
+        return this.alphabet[(26 + index + shift) % 26];
+    }
+    return char;
+  }
+
+  cipherText(text, key, encrypt = true) {
+    if (!text || !key) {
+        throw new Error('Incorrect arguments!');
+    }
+    
+    key = key.toUpperCase();
+    let cipheredText = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        if (this.alphabet.includes(text[i].toUpperCase())) {
+            let shift = this.alphabet.indexOf(key[keyIndex % key.length]);
+            cipheredText += this.shiftChar(text[i], shift, encrypt);
+            keyIndex++;
+        } else {
+            cipheredText += text[i];
+        }
+    }
+    return (this.reverse ? cipheredText.split('').reverse().join('') : cipheredText).toUpperCase();
+  }
+
+  encrypt(text, key) {
+    return this.cipherText(text, key, true);
+  }
+
+  decrypt(text, key) {
+    return this.cipherText(text, key, false);
   }
 }
 
